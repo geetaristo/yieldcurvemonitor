781e653b6c924399b88440a3594824526799d8f8
Michael Luttrell <michael.luttrell@gmail.com>
2018-02-11 16:49:38 -0700

    Fuck vue.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import store from './vuex/store'
+// import store from './vuex/store'
+import Vuex from 'vuex'
 import router from './router'
 import App from './App'
+
+Vue.use(Vuex)
+
+const state = {
+  treasurydata: 'empty data' // {oneYear: ['test data']}
+}
+
+const mutations = {
+  ADD_TREASURY_DATA (state, treasurydata) {
+    state.treasurydata = treasurydata
+  }
+}
+
+const store = new Vuex.Store({
+  state,
+  mutations
+})
 
 Vue.config.productionTip = false
 
